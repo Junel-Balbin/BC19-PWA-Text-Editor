55473c23823c9148547c6749fe0d27ff3a9f9d15
Junel <robin.lennuj@gmail.com>
2023-09-18 15:30:24 -0700

    First edit to the src-sw.js.

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,47 +26,22 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
-
-
-
-
-/* INSTRUCTOR
+// Implement asset caching.
 registerRoute(
-  ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'my-image-cache',
+  ({ request }) => ['image', 'style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
       new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],
   })
 );
- */
 
-/* SOLVED
-// Set up asset cache
-registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    // Name of the cache storage.
-    cacheName: 'asset-cache',
-    plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
-);
-*/
 
 
 // Template Structure and Code Snippets from Mini Project 19.
